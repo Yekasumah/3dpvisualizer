e650b7c571bc6308220e962de2ccc69b510a709c
Yekasumah <asumahyeka@gmail.com>
2023-10-30 03:29:25 +0100

    added preload example

--- pv.py
+++ pv.py
@@ -10,15 +10,23 @@ import io
 import py3Dmol
 
 st.markdown(
-    ''' # 3D Structure Visualization Tool using Speckmol'''
+    ''' # 3D Structure Visualization Tool using Speckmol
+    ### Upload a file from the sidebar to visualize the 3D structure'''
 )
- 
+ex_files = glob.glob("mol/*.xyz")
 with st.sidebar.header('Upload your XYZ FILE'):
     uploaded_file = st.sidebar.file_uploader("Upload your input xyz file", type=["xyz"])
-
+with st.sidebar:
+    example_xyz = st.selectbox("Select a molecule",ex_files)
+    f = open(example_xyz,"r")
+    example_xyz = f.read()
+    st.sidebar.info(example_xyz.splitlines()[1])
 if uploaded_file is not None:
     # Read the content of the uploaded file
     protein_data = uploaded_file.read()
+else: 
+    protein_data = example_xyz
+    st.write("Please upload a protein structure file.")
     
 with st.sidebar.expander("Parameters",expanded=True):
     outl = st.checkbox('Outline',value=True)
